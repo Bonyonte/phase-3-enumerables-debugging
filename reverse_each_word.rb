5112b6e2534d83c9cdcd7a4bcd17811b5a79f906
Bonyonte <godfrey.magogo@student.moringaschool.comgit config --global user.name Bonyontegit branch -m maingit branch -M maingit remote add origin git@github.com:Bonyonte/ip_1.gitgit push -u origin maingit config --global user.email godfrey.magogo@student.moringaschool.com>
2022-08-18 16:17:13 +0300

    done

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,1 +1,19 @@
 # Write your code here
+require 'pry'
+
+def reverse_each_word(sentence)
+    words = sentence.split
+    reversed_words = words.map do |word|
+
+    words.reverse
+    
+    end
+
+    reversed_words.join(" ")
+   
+end
+
+binding.pry
+0
+
+ reverse_each_word("Hello there, and how are you?")
